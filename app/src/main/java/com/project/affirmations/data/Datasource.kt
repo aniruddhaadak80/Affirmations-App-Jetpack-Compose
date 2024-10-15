d0a89d13583c241bf4c7c8b9efe26613c7c30f99
Abinash6000 <abinashdash6000@gmail.com>
2024-10-15 15:57:45 +0530

    first commit

---
+++ app/src/main/java/com/project/affirmations/data/Datasource.kt
@@ -0,0 +1,13 @@
+package com.project.affirmations.data
+
+import com.project.affirmations.R
+import com.project.affirmations.model.Affirmation
+
+class Datasource() {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+    }
+}
