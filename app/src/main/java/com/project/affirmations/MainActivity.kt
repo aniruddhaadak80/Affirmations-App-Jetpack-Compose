4343a298f63ace8d7dd14daebedb830f977bed35
aryan <aryananand005@gmail.com>
2024-10-22 20:51:33 +0530

    Added new feature: Authors added

--- app/src/main/java/com/project/affirmations/MainActivity.kt
+++ app/src/main/java/com/project/affirmations/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -16,11 +17,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.project.affirmations.data.Datasource
 import com.project.affirmations.model.Affirmation
 import com.project.affirmations.ui.theme.AffirmationsTheme
@@ -59,9 +63,9 @@ fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Mod
         }
     }
 }
-
+@Preview
 @Composable
-fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+fun AffirmationCard(affirmation: Affirmation = Affirmation(R.string.affirmation10 , R.drawable.image1 , R.string.author1), modifier: Modifier = Modifier) {
     Card(modifier = modifier) {
         Column {
             Image(
@@ -75,9 +79,21 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
             // Use affirmation's stringResourceId for the text
             Text(
                 text = stringResource(id = affirmation.stringResourceId),
-                modifier = Modifier.padding(16.dp),
+                modifier = Modifier.padding(top = 16.dp , bottom = 8.dp , start = 16.dp , end = 16.dp),
                 style = MaterialTheme.typography.headlineSmall
             )
+            Text(
+                text = "~ "+stringResource(id = affirmation.authorResourceId),
+                fontSize = 15.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End)
+                    .padding(8.dp),
+                style = MaterialTheme.typography.labelMedium,
+            )
+
+
+
         }
     }
 }
